ef268641f8ac63002850fee82b827db8435f9956
Jian <2756760167@qq.com>
2024-10-25 17:06:17 +0800

    feat: 根据title搜索歌曲功能实现

--- actions/getSongsByTitle.ts
+++ actions/getSongsByTitle.ts
@@ -1,30 +1,27 @@
 import { Song } from '@/types'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import getSongs from './getSongs'
 
 
-const getSongsByUserId = async (): Promise<Song[]> => {
+const getSongsByTitle = async (title: string): Promise<Song[]> => {
     const supabase = createServerComponentClient({
         cookies: cookies
     })
-
-    const {
-        data: sessionData,
-        error: sessionError } = await supabase.auth.getSession()
-
-    if (sessionError) {
-        console.log(sessionError)
-        return []
+    if (!title) {
+        const allSongs = await getSongs()
+        return allSongs
     }
     const { data, error } = await supabase
         .from('songs')
-        .select('*')
-        .eq('user_id', sessionData.session?.user.id)
+        .select("*")
+        .ilike('title', `%${title}%`)
+        .order('created_at', { ascending: false })
 
     if (error) {
-        console.log(error.message)
+        console.log(error)
     }
     return (data as any) || []
 }
 
-export default getSongsByUserId
+export default getSongsByTitle
